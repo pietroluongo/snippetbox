91b9c2e7ec57567a9896a93cd664e1f4f0f03c3f
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2023-08-20 21:51:44 -0300

    Initial DB setup

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # snippetbox
+
 Snippetbox API as seen on "Let's Go!" by Alex Edwards
+
+## What is this?
+
+This is a basic API written in Go, as shown on the book ["Let's Go!"](https://lets-go.alexedwards.net/).
+
+### Database
+
+Just for fun, I've decided to use postgreSQL instead of MySQL.
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 type application struct {
@@ -15,11 +18,19 @@ type application struct {
 func main() {
 	// Parse flags
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "user=web password=pass dbname=postgres sslmode=disable", "Postgres DB string")
 	flag.Parse()
 
 	// Setup Loggers & handlers
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, dbErr := openDB(*dsn)
+
+	if dbErr != nil {
+		errorLog.Fatal(dbErr)
+	}
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -37,4 +48,17 @@ func main() {
 	err := server.ListenAndServe()
 	errorLog.Fatal(err)
 
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
